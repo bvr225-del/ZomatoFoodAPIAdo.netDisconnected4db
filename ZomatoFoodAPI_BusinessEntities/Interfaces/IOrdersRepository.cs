1d821692b0113242530ab612078c667736431ffa
venkat <dell@DESKTOP-S8CP3CH>
2026-04-25 12:40:13 +0530

    userstory-123480-createcrudoperationsForOrdersUsingADO.NET

---
+++ ZomatoFoodAPI_BusinessEntities/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZomatoFoodAPI_BusinessEntities.Models;
+
+namespace ZomatoFoodAPI_BusinessEntities.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        Task<bool> AddOrder(Orders Objord);
+        Task<bool> UpdateOrder(Orders Objord);
+        Task<bool> DeleteOrder(int OrderId);
+        Task<List<Orders>> GetallOrders();
+        Task<Orders> GetOrderById(int Id);  
+    }
+}
