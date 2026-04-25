b0d9d4f6c2a42d9ffccb55720d8e2c4681024e73
venkat <dell@DESKTOP-S8CP3CH>
2026-04-25 12:02:02 +0530

    userstory-123479-createCrudOperationForDepartmentUsingADO.NET

--- ZomatoFoodAPI/Program.cs
+++ ZomatoFoodAPI/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();//register the repository interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();//register the service interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
+//=============================================================================================
+//register the dependency injection for the repository and service layers of the application in the program.cs file of the web api project using the AddScoped method   builder object. The AddScoped method is used to register a service with a scoped lifetime, which means that a new instance of the service will be created for each HTTP request and shared within that request.
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();//register the repository interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();//register the service interface and its implementation in the dependency injection container of the application using the AddScoped method   builder object.
 
 var app = builder.Build();
 
---
+++ ZomatoFoodAPI_BusinessEntities/Interfaces/IDepartmentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZomatoFoodAPI_BusinessEntities.Dtos;
+
+namespace ZomatoFoodAPI_BusinessEntities.Interfaces
+{
+    public interface IDepartmentService
+    {
+        Task<List<DepartmentDto>> GetAllDepartments();
+        Task<DepartmentDto> GetDepartmentById(int DepartmentId);
+        //Task<DepartmentDto> GetDepartmentByName(string DepartmentName);
+        Task<bool> AddDepartment(DepartmentDto Dept);
+        Task<bool> UpdateDepartment(DepartmentDto Dept);
+        Task<bool> DeleteDepartment(int DepartmentId);
+
+    }
+}
